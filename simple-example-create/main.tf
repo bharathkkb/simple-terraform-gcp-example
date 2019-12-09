74282962b2a9540f5d10d4639f9768bf4dc85566
bharathkkb <bharathkrishnakb@gmail.com>
2019-12-08 20:10:58 -0600

    add vpc

--- simple-example-create/main.tf
+++ simple-example-create/main.tf
@@ -1,12 +1,20 @@
 provider "google" {
   version = "~> 2.18.0"
 }
-
+module "gcp-network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 1.4.0"
+  project_id   = var.project_id
+  network_name = "test-vpc"
+}
 resource "google_compute_instance" "example_vm" {
   project      = var.project_id
   zone         = "us-central1-a"
   name         = "example-vm"
   machine_type = "f1-micro"
+  network_interface {
+    network = module.gcp-network.network_name
+  }
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
